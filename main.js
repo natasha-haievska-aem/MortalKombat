343a4934df2d7206340e507649d7193f52ce2ed5
NatalyGraham <natalygraham2606@gmail.com>
2021-11-25 09:25:30 +0200

    updated
    забыла убрать ненужный импорт в main.js((

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import {$arena, $formFight, $chat} from "./elements/elements.js";
-import Player from "./entities/Player.js";
-import {ATTACK} from "./constants/attackType.js";
-import fight from "./gameProcess/fight.js";
-import createStartLog from "./gameProcess/servises/start.js";
-import {createElement} from "./utils/elementCreators.js";
-import {player1, player2} from './constants/playersData.js'
-import randomizer from "./utils/randomizer.js";
-
 import Game from "./entities/Game.js";
 
 const game = new Game();
